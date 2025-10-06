0253b7da98cc5eda229dfd80e9088f40ebc5765e
Alvaro Silva <102283382+BabyScylla69@users.noreply.github.com>
2025-10-06 13:49:16 +0200

    refactored cursor in DAO

--- carrerasDAO.py
+++ carrerasDAO.py
@@ -7,68 +7,60 @@ class CarrerasDAO:
         self.__db = self.__dbconnector.connectToDB()
         self.__carrera = Carrera()
 
-    def selectAllCarreras(self):
-        dbCursor = self.__db.cursor()
-        sql = "SELECT * FROM carreras"
-        dbCursor.execute(sql)
-        tuplaCarreras = dbCursor.fetchall()
+    def selectAllCarreras(self): 
+        with self.__db.cursor() as dbCursor:
+            sql = "SELECT * FROM carreras"
+            dbCursor.execute(sql)
+            tuplaCarreras = dbCursor.fetchall()
 
-        for tupla in tuplaCarreras:
+            for tupla in tuplaCarreras:
+                self.__carrera.setId(tupla[0])
+                self.__carrera.setTitulo(tupla[1])
+                self.__carrera.setRama(tupla[3])
+                self.__carrera.setDuracion(tupla[2])
+                self.__carrera.setCampus(tupla[4])
+                carreraARR += [self.__carrera]
+            
+            return carreraARR
+
+    def selectCarreraByID(self, carrera: Carrera):
+        with self.__db.cursor() as dbCursor:
+            sql = "SELECT * FROM carreras c WHERE c.idCarrera = %s"
+            values = (carrera.getId(),)
+            dbCursor.execute(sql, values)
+
+            tupla = dbCursor.fetchall()
             self.__carrera.setId(tupla[0])
             self.__carrera.setTitulo(tupla[1])
             self.__carrera.setRama(tupla[3])
             self.__carrera.setDuracion(tupla[2])
             self.__carrera.setCampus(tupla[4])
-            carreraARR += [self.__carrera]
-        dbCursor.close()
-        
-        return carreraARR
-
-    def selectCarreraByID(self, carrera: Carrera):
-        dbCursor = self.__db.cursor()
-
-        sql = "SELECT * FROM carreras c WHERE c.idCarrera = %s"
-        values = (carrera.getId(),)
-        dbCursor.execute(sql, values)
-
-        tupla = dbCursor.fetchall()
-        self.__carrera.setId(tupla[0])
-        self.__carrera.setTitulo(tupla[1])
-        self.__carrera.setRama(tupla[3])
-        self.__carrera.setDuracion(tupla[2])
-        self.__carrera.setCampus(tupla[4])
-        dbCursor.close()
-
-        return self.__carrera
+            return self.__carrera
     
     def modifySelectedCarrera(self, carrera: Carrera):
-        dbCursor = self.__db.cursor()
-        sql = "UPDATE carreras SET Titulo = %s, Duracion = %s, Rama = %s, Campus = %s WHERE idCarrera = %s"
-        values = (carrera.getTitulo(), carrera.getDuracion(), carrera.getRama(), carrera.getCampus(), carrera.getId())
-        dbCursor.execute(sql, values)
-
-        self.__db.commit()
-        dbCursor.close()
-
-        return carrera
+        with self.__db.cursor() as dbCursor:
+            sql = "UPDATE carreras SET Titulo = %s, Duracion = %s, Rama = %s, Campus = %s WHERE idCarrera = %s"
+            values = (carrera.getTitulo(), carrera.getDuracion(), carrera.getRama(), carrera.getCampus(), carrera.getId())
+            dbCursor.execute(sql, values)
+            self.__db.commit()
+            return carrera
     
     def insertCarrera(self, carrera: Carrera):
-        dbCursor = self.__db.cursor()
-        sql = "INSERT INTO carreras (Titulo, Duracion, Rama, Campus) VALUES (%s, %s, %s, %s);"
-        values = (carrera.getTitulo(), carrera.getDuracion(), carrera.getRama(), carrera.getCampus())
-        dbCursor.execute(sql, values)
-        self.__db.commit()
-        dbCursor.close()
-        return self.checkRows(dbCursor.rowcount)
+        with self.__db.cursor() as dbCursor:
+            sql = "INSERT INTO carreras (Titulo, Duracion, Rama, Campus) VALUES (%s, %s, %s, %s);"
+            values = (carrera.getTitulo(), carrera.getDuracion(), carrera.getRama(), carrera.getCampus())
+            dbCursor.execute(sql, values)
+            self.__db.commit()
+            return self.checkRows(dbCursor.rowcount)
     
     def deleteCarrera(self, carrera: Carrera):
-        dbCursor = self.__db.cursor()
-        sql = "DELETE FROM carreras WHERE idCarrera = %s"
-        values = (carrera.getId(),)
-        dbCursor.execute(sql, values)
-        self.__db.commit()
-        dbCursor.close()
-        return self.checkRows(dbCursor.rowcount)
+        with self.__db.cursor() as dbCursor:
+            dbCursor = self.__db.cursor()
+            sql = "DELETE FROM carreras WHERE idCarrera = %s"
+            values = (carrera.getId(),)
+            dbCursor.execute(sql, values)
+            self.__db.commit()
+            return self.checkRows(dbCursor.rowcount)
 
     def checkRows(self, count):
         if count > 0:
